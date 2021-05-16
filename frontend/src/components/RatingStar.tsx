5aab368873440bdafb855a9993cc6db322c99a69
Michael Hanzel <41654851+Mikahanz@users.noreply.github.com>
2021-05-16 13:38:59 -0400

    upgrade the star to show empty star

--- frontend/src/components/RatingStar.tsx
+++ frontend/src/components/RatingStar.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 
 const RatingStar: React.FC = () => {
-  const num = 4.8;
-
+  const num = 4;
   const evenNum = Math.floor((num * 10) / 10);
   console.log(evenNum);
 
   const numArray = arrOfNum(evenNum);
 
+  const activeStar = isEven(num) ? evenNum : evenNum + 1;
+
+  const unactiveNumStar = 5 - activeStar;
+  const unactiveNumStarArr = arrOfNum(unactiveNumStar);
+
   return (
-    <div>
+    <div className='text-center'>
       {numArray.map((n) => (
         <i className='fa fa-star' style={styleColor} key={n.toString()}></i>
       ))}
       {!isEven(num) && (
         <i className='fa fa-star-half-alt' style={styleColor}></i>
       )}
+      {unactiveNumStarArr.map((n) => (
+        <i className='far fa-star'></i>
+      ))}
     </div>
   );
 };
